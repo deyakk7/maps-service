2e1be667e4cb4582af4707cb3432760fd0338e5b
deyakk <bigboss077701@gmail.com>
2023-10-04 21:31:36 +0300

    fixed

--- root/settings.py
+++ root/settings.py
@@ -112,13 +112,19 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT =  BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
