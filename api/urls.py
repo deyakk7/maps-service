e3b5581e8bb4bd84743fec92563d3a42f674dab4
deyakk <bigboss077701@gmail.com>
2023-10-05 17:28:46 +0300

    changed

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.urls import path
 
 urlpatterns = [
-    path('hello/', lambda request: HttpResponse('Hello World!'))
+    path('hello/', lambda request: HttpResponse('Hello Wdfdsfsdforld!'))
 
 ]
