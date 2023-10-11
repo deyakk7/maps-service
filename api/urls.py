9a4c15e4c765c86925bc9973b08ad2efcc71013e
deyakk <bigboss077701@gmail.com>
2023-10-11 20:05:36 +0300

    try to fix

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('hello/', lambda request: HttpResponse('Hello Wdfdsfsdforld!')),
-    path('schema', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
