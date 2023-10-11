13fbb697ccc70a01f7758b8d5706821857000976
deyakk <bigboss077701@gmail.com>
2023-10-11 20:56:00 +0300

    changed text, training on product

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 # changed one more try
 urlpatterns = [
-    path('hello/', lambda request: HttpResponse('Hello Wdfdsfsdforld!')),
+    path('hello/', lambda request: HttpResponse('Hello Okey let"s change it!')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
