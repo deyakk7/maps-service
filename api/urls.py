ebdc9b3ca03acce9a4f701bd3026b3fc4c4528d3
deyakk <bigboss077701@gmail.com>
2023-10-17 21:32:07 +0300

    added auth with djoser

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,21 @@
 
 from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
 
-# changed one more try
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
-    path('hello/', lambda request: HttpResponse('Hello Okey let"s change it!')),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
-]
+
+    path(r'auth/', include('djoser.urls')),
+    re_path('auth/', include('djoser.urls.authtoken')),
+] +router.urls 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
