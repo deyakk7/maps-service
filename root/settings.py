3276be1bd3fc5c84716b02bc5c5644eb3ad5deb8
deyakk <deiak.yevhenii@gmail.com>
2023-11-20 23:45:30 +0200

    fixed '/api' problem with static and media files

--- root/settings.py
+++ root/settings.py
@@ -144,10 +144,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/api/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
