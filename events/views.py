72e0b0f981056d40bfdf8e406bdbc776c91b72b4
deyakk <deiak.yevhenii@gmail.com>
2023-12-01 15:25:16 +0200

    Added deleting events when end time expires

--- events/views.py
+++ events/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.utils import timezone
 from rest_framework import generics
 from rest_framework import permissions
 
@@ -7,12 +10,15 @@ from api.permissions import IsOwnerOrReadOnly
 
 
 class EventsListView(generics.ListCreateAPIView):
-    queryset = Event.objects.all()
     serializer_class = EventsSerializer
     permission_classes = (permissions.IsAuthenticated, )
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        Event.objects.filter(end_date__lt=datetime.now(timezone.utc)).delete()
+        return Event.objects.all()
 
 
 class EventsDetailView(generics.RetrieveUpdateDestroyAPIView):
