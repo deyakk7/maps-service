59e9f78658e45a5cdf38d89ac29d51406020595b
deyakk <deiak.yevhenii@gmail.com>
2023-12-09 18:35:27 +0200

    Fix event.save() method

--- events/admin.py
+++ events/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Event, Review
 
-admin.site.register(Event)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'is_expired', 'total_reviews', 'rating', 'user')
+    list_filter = ('is_expired', 'user')
+    search_fields = ('title', 'description')
+    ordering = ('-created_at',)
+    readonly_fields = ('is_expired', 'total_reviews', 'rating')
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(Review)
