008f82799ba9c1c0f22bd81ada08e6d470453450
deyakk <deiak.yevhenii@gmail.com>
2023-12-27 18:45:56 +0200

    Add route event/my

--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import (EventsListView, EventsDetailView,
-                    ReviewListView, ReviewDetailView)
+                    ReviewsListView, ReviewsDetailView,
+                    EventsForCurrentUserList)
 
 urlpatterns = [
     path('', EventsListView.as_view(), name='events-list'),
+    path('my/', EventsForCurrentUserList.as_view(), name='events-for-user'),
     path('<int:pk>/', EventsDetailView.as_view(), name='events-detail'),
-    path('reviews/', ReviewListView.as_view(), name='reviews-list'),
-    path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='reviews-detail'),
+    path('reviews/', ReviewsListView.as_view(), name='reviews-list'),
+    path('reviews/<int:pk>/', ReviewsDetailView.as_view(), name='reviews-detail'),
 ]
--- root/settings.py
+++ root/settings.py
@@ -205,3 +205,16 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'JSON_EDITOR': True,
+}
